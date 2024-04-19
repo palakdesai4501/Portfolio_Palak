934c054f4a794c877b2ba2f31acc8023e1d14d14
Palak <palak.desai4501@gmail.com>
2024-04-19 19:39:04 -0400

    Feature: Added three Youtube icons

--- portfolio_palak/components/Header.tsx
+++ portfolio_palak/components/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>I am a Header</header>
-  )
+    <header>
+      <div className="flex flex-row items-center">
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://www.youtube.com/palakdesai"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/palakdesai"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/palakdesai"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+    </header>
+  );
 }
