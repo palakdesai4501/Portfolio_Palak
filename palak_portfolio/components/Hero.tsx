b580a4ff915d584c271dd23315a52fc62cdacffe
Palak <palak.desai4501@gmail.com>
2024-04-22 13:17:37 -0400

    Added use-typewriter hook

--- palak_portfolio/components/Hero.tsx
+++ palak_portfolio/components/Hero.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 type Props = {}
 
 const Hero = (props: Props) => {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi! I am Palak Desai",
+            "Believes in",
+            "Eat-Sleep-Code-Repeat",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
   return (
-    <div>Hero</div>
+    <div>
+        <h1>
+            <span>{text}</span>
+            <Cursor cursorColor='#F7AB0A'/>
+        </h1>
+    </div>
   )
 }
 
--- palak_portfolio/pages/index.tsx
+++ palak_portfolio/pages/index.tsx
@@ -6,7 +6,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Palak's Portfolio</title>
       </Head>
