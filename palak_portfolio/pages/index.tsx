908b1d6e27986698dbc354977868c40bc123dd71
Palak <palak.desai4501@gmail.com>
2024-04-23 00:15:32 -0400

    Added overflow-scroll and snap-center

--- palak_portfolio/pages/index.tsx
+++ palak_portfolio/pages/index.tsx
@@ -6,7 +6,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Palak's Portfolio</title>
       </Head>
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
 
