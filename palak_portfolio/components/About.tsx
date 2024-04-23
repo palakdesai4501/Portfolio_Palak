5b30ee395472e64c99165980eedebed756657778
Palak <palak.desai4501@gmail.com>
2024-04-23 15:10:52 -0400

    Added about profile

--- palak_portfolio/components/About.tsx
+++ palak_portfolio/components/About.tsx
@@ -1,10 +1,32 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import palak_profile from "../public/About_Profile.jpg"
+
 
 type Props = {}
 
 const About = (props: Props) => {
   return (
-    <div>About</div>
+    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+
+        <motion.img 
+            initial={{
+                x: -200,
+                opacity: 0,
+            }}
+            transition={{
+                duration: 1.2,
+            }}
+            whileInView={{ opacity:1, x: 0}}
+            viewport={{ once: true}}
+            src="E:/portfolio/palak_portfolio/public/About_Profile.jpg"
+            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+            md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+        />
+
+               
+    </div>
   )
 }
 
