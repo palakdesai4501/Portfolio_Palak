c47a9b3ef514e92ce558bb9896abfe834956eb58
Palak <palak.desai4501@gmail.com>
2024-04-23 14:12:34 -0400

    Added buttons

--- palak_portfolio/components/Hero.tsx
+++ palak_portfolio/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
+import Image from 'next/image'
+import profile from '../public/profile.jpg'
 
 type Props = {}
 
@@ -17,10 +19,26 @@ const Hero = (props: Props) => {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles />
-        <h1>
+        <Image className='relative rounded-full h-32 w-32 mx-auto object-cover' 
+        src={profile} 
+        alt="profile picture"/>
+        <div>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+                Software Engineer
+            </h2>
+        </div>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
             <span>{text}</span>
             <Cursor cursorColor='#F7AB0A'/>
         </h1>
+
+        <div>
+            <button className='heroButton'>About</button>
+            <button className='heroButton'>Experience</button>
+            <button className='heroButton'>Skills</button>
+            <button className='heroButton'>Projects</button>
+        </div>
+
     </div>
   )
 }
