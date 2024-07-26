c3a4c1e23c138ce2382f697602aef808da61f8e9
Palak <palak.desai4501@gmail.com>
2024-07-25 23:14:55 -0400

    Created projects component

--- palak_portfolio/pages/index.tsx
+++ palak_portfolio/pages/index.tsx
@@ -1,6 +1,7 @@
 import About from '@/components/About'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 import WorkExperience from '@/components/WorkExperience'
 import Head from 'next/head'
@@ -36,6 +37,10 @@ const index = (props: Props) => {
     {/* Skills */}
     <section id='skills' className='snap-start'>
       <Skills />
+    </section>
+
+    <section id="projects" className='snap-start'>
+      <Projects />
     </section>
 
   </div>
