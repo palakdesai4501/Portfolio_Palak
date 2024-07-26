81791dc119fdeaf16fc9c5c433f69bbcb5028b7b
Palak <palak.desai4501@gmail.com>
2024-07-26 01:54:46 -0400

    set size and padding of projects section of small device

--- palak_portfolio/components/Projects.tsx
+++ palak_portfolio/components/Projects.tsx
@@ -14,7 +14,7 @@ function Projects({}: Props) {
         className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
-        <div className='relative w-full flex overflow-x-scroll space-y-5 overflow-hidden snap-x snap-mandatory z-20 mt-52'>
+        <div className='relative w-full flex overflow-x-scroll space-y-5 overflow-hidden snap-x snap-mandatory z-20 mt-40'>
            {projects.map((project, i) => (
             <div className='w-screen flex-shrink-0 snap-center flex flex-col   items-center justify-center p-20 md:p-44 h-screen '>
                 <motion.img 
@@ -25,7 +25,7 @@ function Projects({}: Props) {
                     transition={{ duration: 1.2}}
                     whileInView={{ opacity: 1, y: 0}}
                     viewport={{ once: true}}
-                    src="/yt.png" alt="youtube-clone" className='xl: size-96' />
+                    src="/yt.png" alt="youtube-clone" className='xl:size-96 sm: size-60' />
 
                 <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                     <h4 className='text-2xl font-semibold text-center'>
