456c824ec78735da2e67782203c8d1a377146ef4
Palak <palak.desai4501@gmail.com>
2024-07-27 00:41:15 -0400

    Created form in contact me

--- palak_portfolio/components/ContactMe.tsx
+++ palak_portfolio/components/ContactMe.tsx
@@ -30,6 +30,18 @@ function ContactMe({}: Props) {
             <p className='text-2xl'>539 Campbell Ave</p>
           </div>
         </div>
+
+        <form className='flex flex-col space-y-2 w-fit mx-auto'>
+          <div className='flex space-x-2'>
+            <input className= "contactInput" type="text" />
+            <input className= "contactInput" type="text" />
+          </div>
+
+          <input className= "contactInput" type="text" />
+
+          <textarea className="contactInput" />
+          <button>Submit</button>
+        </form>
       </div>
     </div>
   )
